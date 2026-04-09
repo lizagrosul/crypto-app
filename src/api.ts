c33928e748bcaacc55177e0c5f85415f5f456449
Liza Grosul <liza.grosul.dev@gmail.com>
2026-04-09 21:15:33 +0200

    feat: WebSocket connection

---
+++ src/api.ts
@@ -0,0 +1,61 @@
+const API_KEY = '41a142f4baa7549a2342ceb301f607e8bbd21592699a6d220b6b922906275aac'
+const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`)
+const handlers = new Map<string, Function[]>()
+
+interface TickerData {
+  TYPE: string
+  FROMSYMBOL: string
+  PRICE?: number
+}
+
+socket.addEventListener('open', () => {
+  if (handlers.size > 0) {
+    const tickerNames = Array.from(handlers.keys())
+    sendToSocket({
+      action: 'SubAdd',
+      subs: tickerNames.map((t) => `5~CCCAGG~${t}~USD`),
+    })
+  }
+})
+
+socket.addEventListener('message', (e) => {
+  const data: TickerData = JSON.parse(e.data)
+  const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = data
+  if (type !== '5' || newPrice === undefined) {
+    return
+  }
+
+  const currencyHandlers = handlers.get(currency) || []
+  currencyHandlers.forEach((fn) => {
+    fn(newPrice)
+  })
+})
+
+function sendToSocket(message: object) {
+  const stringifiedMessage = JSON.stringify(message)
+  if (socket?.readyState === WebSocket.OPEN) {
+    socket.send(stringifiedMessage)
+    return
+  }
+}
+
+export const subscribeToTicker = (ticker: string, cb: Function) => {
+  if (!cb) return
+
+  const subscribers = handlers.get(ticker) || []
+  if (subscribers.length === 0 && socket.readyState === WebSocket.OPEN) {
+    sendToSocket({
+      action: 'SubAdd',
+      subs: [`5~CCCAGG~${ticker}~USD`],
+    })
+  }
+  handlers.set(ticker, [...subscribers, cb])
+}
+
+export const unsubscribeFromTicker = (ticker: string) => {
+  handlers.delete(ticker)
+  sendToSocket({
+    action: 'SubRemove',
+    subs: [`5~CCCAGG~${ticker}~USD`],
+  })
+}
--- src/stores/crypto.ts
+++ src/stores/crypto.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import type { Ticker } from '@/types/ticker'
+import { subscribeToTicker, unsubscribeFromTicker } from '@/api'
 
 export const useCryptoStore = defineStore('crypto', () => {
   const tickers = ref<Ticker[]>([
@@ -18,6 +19,17 @@ export const useCryptoStore = defineStore('crypto', () => {
     },
   ])
 
+  const updatePrice = (name: string, price: number) => {
+    const currentTicker = tickers.value.find((t) => t.name === name)
+    if (currentTicker) {
+      currentTicker.price = price > 1 ? price.toFixed(2) : price.toPrecision(6)
+    }
+  }
+
+  tickers.value.forEach((t) => {
+    subscribeToTicker(t.name, (newPrice: number) => updatePrice(t.name, newPrice))
+  })
+
   function addTicker(tickerName: string) {
     const formattedName = tickerName.trim().toUpperCase()
     if (!formattedName) return
@@ -31,10 +43,14 @@ export const useCryptoStore = defineStore('crypto', () => {
     }
 
     tickers.value.push(newTicker)
+    subscribeToTicker(formattedName, (newPrice: number) => {
+      updatePrice(formattedName, newPrice)
+    })
   }
 
   function removeTicker(tickerName: string) {
     tickers.value = tickers.value.filter((t) => t.name !== tickerName)
+    unsubscribeFromTicker(tickerName)
   }
 
   return {
