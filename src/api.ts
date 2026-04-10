ced4afc4eaf905a3788690330e4aef24b4eb8549
Liza Grosul <liza.grosul.dev@gmail.com>
2026-04-10 14:13:33 +0200

    feat: add auto-reconnect to WebSocket

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 const API_KEY = '41a142f4baa7549a2342ceb301f607e8bbd21592699a6d220b6b922906275aac'
-const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`)
+//const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`)
+let socket: WebSocket | null = null
 const handlers = new Map<string, Function[]>()
 
 interface TickerData {
@@ -8,28 +9,46 @@ interface TickerData {
   PRICE?: number
 }
 
-socket.addEventListener('open', () => {
-  if (handlers.size > 0) {
-    const tickerNames = Array.from(handlers.keys())
-    sendToSocket({
-      action: 'SubAdd',
-      subs: tickerNames.map((t) => `5~CCCAGG~${t}~USD`),
-    })
-  }
-})
+function createSocketConnection() {
+  if (socket) return
+  socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`)
 
-socket.addEventListener('message', (e) => {
-  const data: TickerData = JSON.parse(e.data)
-  const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = data
-  if (type !== '5' || newPrice === undefined) {
-    return
-  }
-
-  const currencyHandlers = handlers.get(currency) || []
-  currencyHandlers.forEach((fn) => {
-    fn(newPrice)
+  socket.addEventListener('open', () => {
+    if (handlers.size > 0) {
+      const tickerNames = Array.from(handlers.keys())
+      sendToSocket({
+        action: 'SubAdd',
+        subs: tickerNames.map((t) => `5~CCCAGG~${t}~USD`),
+      })
+    }
   })
-})
+
+  socket.addEventListener('message', (e) => {
+    const data: TickerData = JSON.parse(e.data)
+    const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = data
+    if (type !== '5' || newPrice === undefined) {
+      return
+    }
+
+    const currencyHandlers = handlers.get(currency) || []
+    currencyHandlers.forEach((fn) => {
+      fn(newPrice)
+    })
+  })
+
+  socket.addEventListener('close', () => {
+    console.warn('Socket closed. Reconnecting in 10s')
+    socket = null
+    setTimeout(createSocketConnection, 10000)
+  })
+
+  socket.addEventListener('error', (e) => {
+    console.error('WebSocket error:', e)
+    socket?.close()
+  })
+}
+
+createSocketConnection()
 
 function sendToSocket(message: object) {
   const stringifiedMessage = JSON.stringify(message)
@@ -43,7 +62,7 @@ export const subscribeToTicker = (ticker: string, cb: Function) => {
   if (!cb) return
 
   const subscribers = handlers.get(ticker) || []
-  if (subscribers.length === 0 && socket.readyState === WebSocket.OPEN) {
+  if (subscribers.length === 0 && socket?.readyState === WebSocket.OPEN) {
     sendToSocket({
       action: 'SubAdd',
       subs: [`5~CCCAGG~${ticker}~USD`],
